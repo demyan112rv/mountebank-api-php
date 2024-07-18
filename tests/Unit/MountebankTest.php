8522bee0caa4e1c36c696a11c23e9d6778c535b1
Ruslan Demyanovsky <demyan112rv@gmail.com>
2024-07-18 16:59:54 +0200

    fix test

--- tests/Unit/MountebankTest.php
+++ tests/Unit/MountebankTest.php
@@ -26,6 +26,6 @@ class MountebankTest extends TestCase
         $this->assertEquals('http://test.com:1234/' . Mountebank::URI_CONFIG, $mb->getConfigUrl());
         $this->assertEquals('http://test.com:1234/' . Mountebank::URI_LOGS, $mb->getLogsUrl());
         $this->assertEquals('http://test.com:1234/' . Mountebank::URI_IMPOSTERS, $mb->getImpostersUrl());
-        $this->assertEquals('http://test.com:1234/' . Mountebank::URI_IMPOSTERS . '/1234/' . Mountebank::URI_STUBS, $mb->getStubsUrl());
+        $this->assertEquals('http://test.com:1234/' . Mountebank::URI_IMPOSTERS . '/9999/' . Mountebank::URI_STUBS, $mb->getStubsUrl(9999));
     }
 }
